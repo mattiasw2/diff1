4d1d53057ab4c03484c4111003cf2ea1e342cd28
Mattias Waldau <mattias.waldau@excelwraps.com>
2025-01-03 10:30:26 +0100

    Use Theory

--- DiffTests/SimilarButDifferentTests.fs
+++ DiffTests/SimilarButDifferentTests.fs
@@ -2,57 +2,16 @@
 
 open Xunit
 
-// Test cases for the `similarButDifferent` function
-[<Fact>]
-let ``Lines with at least 3 identical and 1 differing characters should be similar but different`` () =
-    let line1 = "F# is fun!"
-    let line2 = "F# is cool!"
-    Assert.True(CompareFiles.similarButDifferent line1 line2)
-
-[<Fact>]
-let ``Lines with less than 3 identical characters should not be similar but different`` () =
-    let line1 = "abc"
-    let line2 = "xyz"
-    Assert.False(CompareFiles.similarButDifferent line1 line2)
-
-[<Fact>]
-let ``Lines with less than 1 differing character should not be similar but different`` () =
-    let line1 = "F# is fun!"
-    let line2 = "F# is fun."
-    Assert.True(CompareFiles.similarButDifferent line1 line2)
-
-[<Fact>]
-let ``Lines with identical content should not be similar but different`` () =
-    let line1 = "Hello, World!"
-    let line2 = "Hello, World!"
-    Assert.False(CompareFiles.similarButDifferent line1 line2)
-
-[<Fact>]
-let ``Lines with no identical characters should not be similar but different`` () =
-    let line1 = "abc"
-    let line2 = "def"
-    Assert.False(CompareFiles.similarButDifferent line1 line2)
-
-[<Fact>]
-let ``Lines with identical characters but no differing characters should not be similar but different`` () =
-    let line1 = "F# is fun!"
-    let line2 = "F# is fun!"
-    Assert.False(CompareFiles.similarButDifferent line1 line2)
-
-[<Fact>]
-let ``Lines with at least 3 identical and 1 differing characters in different positions should be similar but different`` () =
-    let line1 = "F# is fun!"
-    let line2 = "F# is cool!"
-    Assert.True(CompareFiles.similarButDifferent line1 line2)
-
-[<Fact>]
-let ``Lines with at least 3 identical and 1 differing characters but different lengths should be similar but different`` () =
-    let line1 = "F# is fun!"
-    let line2 = "F# is cool!!!"
-    Assert.True(CompareFiles.similarButDifferent line1 line2)
-
-[<Fact>]
-let ``Lines with at least 3 identical and 1 differing characters but reversed should be similar but different`` () =
-    let line1 = "F# is fun!"
-    let line2 = "cool is F#!"
-    Assert.True(CompareFiles.similarButDifferent line1 line2)
+// Test cases for the `similarButDifferent` function using Theory
+[<Theory>]
+[<InlineData("F# is fun!", "F# is cool!", true)>]
+[<InlineData("abc", "xyz", false)>]
+[<InlineData("F# is fun!", "F# is fun.", true)>]
+[<InlineData("Hello, World!", "Hello, World!", false)>]
+[<InlineData("abc", "def", false)>]
+[<InlineData("F# is fun!", "F# is fun!", false)>]
+[<InlineData("F# is fun!", "F# is cool!", true)>]
+[<InlineData("F# is fun!", "F# is cool!!!", true)>]
+[<InlineData("F# is fun!", "cool is F#!", true)>]
+let ``Test SimilarButDifferent`` (line1: string, line2: string, expected: bool) =
+    Assert.Equal(expected, CompareFiles.similarButDifferent line1 line2)
