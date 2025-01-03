4a31e87bc0414ba8f2d526d9655606c7624406d6
Mattias Waldau <mattias.waldau@excelwraps.com>
2025-01-03 09:27:44 +0100

    tell user test files were generated

--- DiffTests/Program.fs
+++ DiffTests/Program.fs
@@ -2,6 +2,7 @@
 
 [<EntryPoint>]
 let main _ = 
+    System.Console.WriteLine("Create test data files c:/temp/file1.txt and c:/temp/big1.txt")
     let _ = FileGenerator.generateFiles 1000 80 50 42 "c:/temp/file1.txt" "c:/temp/file2.txt"
     let _ = FileGenerator.generateFiles 100000 1000 5000 43 "c:/temp/big1.txt" "c:/temp/big2.txt"
     0
