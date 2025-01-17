ed5d1162d66feea65bff0c0296b9ced2c734d940
Mattias Waldau <mattias.waldau@excelwraps.com>
2025-01-17 17:00:27 +0100

    Sort unit test to remove duplicates to get rid of xunit warning

--- tests/DiffTests/HighlightDifferencesTests.fs
+++ tests/DiffTests/HighlightDifferencesTests.fs
@@ -4,43 +4,42 @@ open Xunit
 
 // Test cases for the `highlightDifferences` function
 [<Theory>]
-[<InlineData("abcd", "abc", "abc(d|)")>] // Extra character at the end
-[<InlineData("abc", "abcd", "abc(|d)")>] // Extra character at the end
-[<InlineData("foo", "bar", "(foo|bar)")>] // Completely different lines
-[<InlineData("xfooy", "xbary", "x(foo|bar)y")>] // Differences in the middle
-[<InlineData("abc", "def", "(abc|def)")>] // Completely different lines
-[<InlineData("abc", "abc", "abc")>] // Identical lines
+[<InlineData("  abc  ", "  xyz  ", "  (abc|xyz)  ")>] // Preserve whitespace
+[<InlineData("", "", "")>]  // Empty strings to make extractDifferences return ("","","")
+[<InlineData("", "abc", "(|abc)")>] // One empty, one non-empty
 [<InlineData("ab", "abc", "ab(|c)")>] // Extra character at the end
+[<InlineData("abc", "", "(abc|)")>] // One non-empty, one empty
 [<InlineData("abc", "ab", "ab(c|)")>] // Extra character at the end
-[<InlineData("abcde", "axcye", "a(bcd|xcy)e")>] // Multiple differences
+[<InlineData("abc", "abc", "abc")>] // Identical lines
+[<InlineData("abc", "abcd", "abc(|d)")>] // Extra character at the end
+[<InlineData("abc", "abz", "ab(c|z)")>] // Single difference at the end
 [<InlineData("abc", "axc", "a(b|x)c")>] // Single difference in the middle
 [<InlineData("abc", "ayc", "a(b|y)c")>] // Single difference in the middle
-[<InlineData("abc", "abz", "ab(c|z)")>] // Single difference at the end
+[<InlineData("abc", "def", "(abc|def)")>] // Completely different lines
 [<InlineData("abc", "zbc", "(a|z)bc")>] // Single difference at the start
+[<InlineData("abcd", "abc", "abc(d|)")>] // Extra character at the end
+[<InlineData("abcde", "axcye", "a(bcd|xcy)e")>] // Multiple differences
+[<InlineData("foo", "bar", "(foo|bar)")>] // Completely different lines
+[<InlineData("Hello test! Bye", "Hello test! Bye", "Hello test! Bye")>] 
+[<InlineData("Hello test! Bye", "Hello world! Bye", "Hello (test|world)! Bye")>]
 [<InlineData("hello world", "hello there", "hello (world|there)")>] // Multi-word difference
+[<InlineData("Hello🌍", "Hello🌎", "Hello(🌍|🌎)")>] // Emojis
+[<InlineData("one two three", "one two four", "one two (three|four)")>] // Multi-word difference
+[<InlineData("prefix abcd suffix", "prefix abcd suffix", "prefix abcd suffix")>]  // Identical strings to make extractDifferences return ("abcd","","")
+[<InlineData("prefix abcde suffix", "prefix fghij suffix", "prefix (abcde|fghij) suffix")>]  // Long diffs (>3 chars) with no common substrings
+[<InlineData("prefix middle1 suffix", "prefix middle1 suffix", "prefix middle1 suffix")>] 
+[<InlineData("prefix middle1 suffix", "prefix middle2 suffix", "prefix middle(1|2) suffix")>]
+[<InlineData("start 123 end", "start 123 end", "start 123 end")>] 
+[<InlineData("start 123 end", "start 456 end", "start (123|456) end")>]
+[<InlineData("start 12345 end", "start 12345 end", "start 12345 end")>]  // Another case with identical strings
+[<InlineData("start 12345 end", "start 67890 end", "start (12345|67890) end")>]  // Another case with long diffs (>3 chars)
 [<InlineData("the quick brown fox", "the slow brown fox", "the (quick|slow) brown fox")>] // Multi-word difference
 [<InlineData("this is a test", "this was a test", "this (i|wa)s a test")>] // Multi-word difference
-[<InlineData("one two three", "one two four", "one two (three|four)")>] // Multi-word difference
-[<InlineData("", "", "")>] // Empty strings
-[<InlineData("", "abc", "(|abc)")>] // One empty, one non-empty
-[<InlineData("abc", "", "(abc|)")>] // One non-empty, one empty
+[<InlineData("xfooy", "xbary", "x(foo|bar)y")>] // Differences in the middle
 [<InlineData("αβγ", "αδγ", "α(β|δ)γ")>] // Unicode characters
-[<InlineData("Hello🌍", "Hello🌎", "Hello(🌍|🌎)")>] // Emojis
-[<InlineData("👨‍💻 coding", "👩‍💻 coding", "(👨‍💻|👩‍💻) coding")>] // Complex emojis with ZWJ - compare complete sequences
-[<InlineData("  abc  ", "  xyz  ", "  (abc|xyz)  ")>] // Preserve whitespace
-[<InlineData("prefix abcd suffix", "prefix abcd suffix", "prefix abcd suffix")>]  // Identical strings to make extractDifferences return ("abcd","","")
-[<InlineData("start 12345 end", "start 12345 end", "start 12345 end")>]  // Another case with identical strings
-[<InlineData("prefix middle1 suffix", "prefix middle1 suffix", "prefix middle1 suffix")>] 
-[<InlineData("start 123 end", "start 123 end", "start 123 end")>] 
 [<InlineData("🌍 abc 🌎", "🌍 abc 🌎", "🌍 abc 🌎")>] 
-[<InlineData("Hello test! Bye", "Hello test! Bye", "Hello test! Bye")>] 
-[<InlineData("", "", "")>]  // Empty strings to make extractDifferences return ("","","")
-[<InlineData("prefix abcde suffix", "prefix fghij suffix", "prefix (abcde|fghij) suffix")>]  // Long diffs (>3 chars) with no common substrings
-[<InlineData("start 12345 end", "start 67890 end", "start (12345|67890) end")>]  // Another case with long diffs (>3 chars)
-[<InlineData("prefix middle1 suffix", "prefix middle2 suffix", "prefix middle(1|2) suffix")>]
-[<InlineData("start 123 end", "start 456 end", "start (123|456) end")>]
 [<InlineData("🌍 abc 🌎", "🌍 xyz 🌎", "🌍 (abc|xyz) 🌎")>]
-[<InlineData("Hello test! Bye", "Hello world! Bye", "Hello (test|world)! Bye")>]
+[<InlineData("👨‍💻 coding", "👩‍💻 coding", "(👨‍💻|👩‍💻) coding")>] // Complex emojis with ZWJ - compare complete sequences
 let ``highlightDifferences should correctly highlight differences`` (left: string) (right: string) (expected: string) =
     let result = CompareFiles.highlightDifferences left right
     Assert.Equal(expected, result)
--- tests/DiffTests/SimilarButDifferentTests.fs
+++ tests/DiffTests/SimilarButDifferentTests.fs
@@ -4,21 +4,20 @@ open Xunit
 
 // Test cases for the `similarButDifferent` function using Theory
 [<Theory>]
-[<InlineData("F# is fun!", "F# is cool!", true)>]
-[<InlineData("abc", "xyz", false)>]
-[<InlineData("F# is fun!", "F# is fun.", true)>]
-[<InlineData("Hello, World!", "Hello, World!", false)>]
-[<InlineData("abc", "def", false)>]
-[<InlineData("F# is fun!", "F# is fun!", false)>]
-[<InlineData("F# is fun!", "F# is cool!", true)>]
-[<InlineData("F# is fun!", "F# is cool!!!", true)>]
-[<InlineData("F# is fun!", "cool is F#!", true)>]
-[<InlineData("Hello", "Hello", false)>]  // Identical strings
 [<InlineData("", "", false)>]  // Empty strings
 [<InlineData("", "nonempty", false)>]  // One empty, one non-empty
 [<InlineData("a", "b", false)>]  // Single different characters
-[<InlineData("αβγ", "αδγ", true)>]  // Unicode characters
+[<InlineData("abc", "def", false)>]
+[<InlineData("abc", "xyz", false)>]
+[<InlineData("F# is fun!", "cool is F#!", true)>]
+[<InlineData("F# is fun!", "F# is cool!!!", true)>]
+[<InlineData("F# is fun!", "F# is cool!", true)>]
+[<InlineData("F# is fun!", "F# is fun!", false)>]
+[<InlineData("F# is fun!", "F# is fun.", true)>]
+[<InlineData("Hello", "Hello", false)>]  // Identical strings
+[<InlineData("Hello, World!", "Hello, World!", false)>]
 [<InlineData("Hello🌍", "Hello🌎", true)>]  // Emojis
 [<InlineData("Very long string that has many characters", "Very long string with different ending", true)>]
+[<InlineData("αβγ", "αδγ", true)>]  // Unicode characters
 let ``Test SimilarButDifferent`` (line1: string, line2: string, expected: bool) =
     Assert.Equal(expected, CompareFiles.similarButDifferent line1 line2)
